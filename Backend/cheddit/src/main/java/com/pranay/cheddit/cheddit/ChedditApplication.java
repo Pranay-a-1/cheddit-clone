a1c581c6a8ea073cf23f20bbdd828d41d42ad398
Pranay Alamalakala <pranay.a4795@gmail.com>
2023-10-30 21:41:26 +0000

    created spring boot backend project

---
+++ Backend/cheddit/src/main/java/com/pranay/cheddit/cheddit/ChedditApplication.java
@@ -0,0 +1,13 @@
+package com.pranay.cheddit.cheddit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChedditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChedditApplication.class, args);
+	}
+
+}
---
+++ Backend/cheddit/src/test/java/com/pranay/cheddit/cheddit/ChedditApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pranay.cheddit.cheddit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChedditApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
